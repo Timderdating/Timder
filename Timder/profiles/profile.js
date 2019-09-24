3036a6c887158300f05f4bb31a241c01374e52fb
tim-huynh <huynt553@gmail.com>
2019-09-23 21:42:11 -0700

    profile data structure

--- Timder/profiles/profile.js
+++ Timder/profiles/profile.js
@@ -1,18 +1,32 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
 
-	// Array of profiles
-	var profiles = ['normal_tim']
+    // Nested profile array
+    var profiles = [{
+        'folder': 'normal_tim',
+        'name': 'Tim Huynh',
+        'bio': "You already know he's a bitch."
+    },
+    {
+        'folder': 'nerdy_tim',
+        'name': 'Nerdy Tim',
+        'bio': 'e=mc2, u = arqt'
+    }];
 
 	// Randomly select a profile
 	var randomIndex = Math.floor((Math.random() * profiles.length));
-	var randomProfile = profiles[randomIndex];
-	randomProfile = randomProfile.toLowerCase();
+	var randomProfile = profiles[randomIndex].folder;
+    var bioText = profiles[randomIndex].bio;
+    var profileName = profiles[randomIndex].name;
 
-	// Enter folder of randomly selected profile
+	// Enter folder of randomly selected profile for images
 	$('img.picture1').attr('src', randomProfile+'/1.jpg');
 	$('img.picture2').attr('src', randomProfile+'/2.jpg');
 	$('img.picture3').attr('src', randomProfile+'/3.jpg');
+
+    // Display bio and name of corresponding profile
+    $('.bioInfo').text(bioText);
+    $('.profName').text(profileName);
 
     $(".likeBtn").on('click', function() {
     	$('#likeModal').modal('show');
