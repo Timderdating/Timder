05e872f4df9eb1629ebc1e71bdc63a4011af229a
tim-huynh <huynt553@gmail.com>
2019-09-23 19:59:31 -0700

    adding profile data structure

--- Timder/profiles/profile.js
+++ Timder/profiles/profile.js
@@ -1,6 +1,19 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
 
+	// Array of profiles
+	var profiles = ['normal_tim']
+
+	// Randomly select a profile
+	var randomIndex = Math.floor((Math.random() * profiles.length));
+	var randomProfile = profiles[randomIndex];
+	randomProfile = randomProfile.toLowerCase();
+
+	// Enter folder of randomly selected profile
+	$('img.picture1').attr('src', randomProfile+'/1.jpg');
+	$('img.picture2').attr('src', randomProfile+'/2.jpg');
+	$('img.picture3').attr('src', randomProfile+'/3.jpg');
+
     $(".likeBtn").on('click', function() {
     	$('#likeModal').modal('show');
     });
